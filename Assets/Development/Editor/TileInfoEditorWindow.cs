ccc402b71a40e07e1caa7aef3d51843c9f5c60e3
778a0a <778a0a@778a0a.com>
2024-10-20 12:43:34 +0900

    エディター キャラ移動処理追加

--- Assets/Development/Editor/TileInfoEditorWindow.cs
+++ Assets/Development/Editor/TileInfoEditorWindow.cs
@@ -16,7 +16,15 @@ public class TileInfoEditorWindow : EditorWindow
     private GameMapTile targetTile;
     private Country targetCountry;
 
+    private bool prevF1;
     private bool isLocked = false;
+    private bool prevF2;
+    private EditMode mode = EditMode.EditBuilding;
+    private enum EditMode
+    {
+        EditBuilding,
+        EditCharacter,
+    }
 
     [MenuItem("開発/タイル情報")]
     public static void ShowWindow()
@@ -41,7 +49,7 @@ public class TileInfoEditorWindow : EditorWindow
             world = GameCore.Instance.World;
             return;
         }
-        
+
         var map = FindFirstObjectByType<UIMapManager>();
         map.Awake();
         world = DefaultData.Create(saveDir);
@@ -54,7 +62,6 @@ public class TileInfoEditorWindow : EditorWindow
         SceneView.duringSceneGui -= DuringSceneGUI;
     }
 
-    private bool prevF1;
     void DuringSceneGUI(SceneView sceneView)
     {
         var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
@@ -104,79 +111,118 @@ public class TileInfoEditorWindow : EditorWindow
             GUI.FocusControl(null);
             Repaint();
         }
+        var oldF2 = prevF2;
+        prevF2 = Keyboard.current.f2Key.isPressed;
+        if (prevF2 && !oldF2)
+        {
+            Debug.Log("Toggle Edit Mode");
+            mode = mode == EditMode.EditBuilding ? EditMode.EditCharacter : EditMode.EditBuilding;
+            GUI.FocusControl(null);
+            Repaint();
+        }
 
+
+        // ヘッダー 保存ボタンなど
         using (HorizontalLayout())
         {
+            Label($"保存先:");
             saveDir = EditorGUILayout.TextField(saveDir, GUILayout.Width(50));
             if (GUILayout.Button("再読込", GUILayout.Width(70)))
             {
                 LoadWorld();
-            }
-            if (GUILayout.Button(isLocked ? "ﾛｯｸ解除" : "ロック",
-                new GUIStyle(GUI.skin.button) { normal = new GUIStyleState() { textColor = isLocked ? Color.yellow : Color.white } },
-                GUILayout.Width(70)))
-            {
-                isLocked = !isLocked;
             }
             if (GUILayout.Button("保存"))
             {
                 Save();
                 LoadWorld();
             }
+
+            var style = new GUIStyle(GUI.skin.button);
+            style.normal.textColor = isLocked ? Color.yellow : Color.white;
+            if (GUILayout.Button(isLocked ? "ﾛｯｸ解除" : "ロック", style))
+            {
+                isLocked = !isLocked;
+            }
+
+            if (GUILayout.Button(mode == EditMode.EditBuilding ? "施設編集" : "キャラ編集"))
+            {
+                mode = mode == EditMode.EditBuilding ? EditMode.EditCharacter : EditMode.EditBuilding;
+            }
         }
 
+        // 有効なタイルでなければ何もしない。
         if (targetTile == null) return;
+
+
         using (HorizontalLayout())
         {
-            GUILayout.Label($"座標: {targetTile.Position} 地形: {targetTile.Terrain}", GUILayout.Width(150));
-            GUILayout.Label($"Gmax: {GameMapTile.TileGoldMax(targetTile):000} Fmax:{GameMapTile.TileFoodMax(targetTile):0000}", GUILayout.Width(150));
+            Label($"座標: {targetTile.Position} 地形: {targetTile.Terrain}", 150);
+            Label($"Gmax: {GameMapTile.TileGoldMax(targetTile):000} Fmax:{GameMapTile.TileFoodMax(targetTile):0000}", 150);
         }
 
         // スクロール可能にする。
         using var _scroll = ScrollView(Vector2.zero);
 
-        var hasCountry = targetCountry != null;
-        if (hasCountry)
+        if (targetTile.Country != null)
         {
-            var ruler = targetCountry.Ruler;
-
-            using var _ = HorizontalLayout();
-
-            using (VerticalLayout(GUILayout.Width(110)))
-            {
-                GUILayout.Label($"国: {targetCountry.Id} {ruler.Name}", GUILayout.Width(110));
-                var rulerImage = FaceImageManager.Instance.GetImage(ruler);
-                GUILayout.Box(rulerImage, new GUIStyle() { fixedWidth = 100, fixedHeight = 100, margin = new(5, 5, 0, 0) });
-            }
-
-            if (targetTile.HasCastle)
-            {
-                var castle = targetTile.Castle;
-                using (VerticalLayout(GUILayout.Width(100)))
-                {
-                    GUILayout.Label($"城主: {castle.Boss?.Name ?? ""}", GUILayout.Width(100));
-                    if (castle.Boss != null)
-                    {
-                        var bossImage = FaceImageManager.Instance.GetImage(targetTile.Castle.Boss);
-                        GUILayout.Box(bossImage, new GUIStyle() { fixedWidth = 100, fixedHeight = 100, margin = new(5, 5, 0, 0) });
-                    }
-                }
-
-                using (VerticalLayout(GUILayout.Width(100)))
-                {
-                    GUILayout.Label($"将数: {castle.Members.Count}", GUILayout.Width(100));
-                    using (HorizontalLayout())
-                    {
-                        foreach (var chara in castle.Members.Where(m => m != castle.Boss))
-                        {
-                            var image = FaceImageManager.Instance.GetImage(chara);
-                            GUILayout.Box(image, new GUIStyle() { fixedWidth = 45, fixedHeight = 45, margin = new(5, 5, 0, 0) });
-                        }
-                    }
-                }
-            }
+            DrawBuildingOverview();
         }
 
+        switch (mode)
+        {
+            case EditMode.EditBuilding:
+                DrawEditBuilding();
+                break;
+            case EditMode.EditCharacter:
+                DrawEditCharacter();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private int moveCharacterCastleId;
+    private void DrawEditCharacter()
+    {
+        var castle = targetTile.Castle;
+        if (castle == null) return;
+
+        BoldLabel("キャラ一覧");
+        foreach (var chara in castle.Members)
+        {
+            GUILayout.Space(5);
+            using var _ = HorizontalLayout();
+            CharaImage(chara);
+
+            using var __ = VerticalLayout();
+            Label($"ID:{chara.Id}\t{chara}");
+
+            using (HorizontalLayout())
+            {
+                Label("城ID:");
+                moveCharacterCastleId = EditorGUILayout.IntField(moveCharacterCastleId);
+                if (GUILayout.Button("へ移動"))
+                {
+                    var newCastle = world.Castles.FirstOrDefault(c => c.Id == moveCharacterCastleId);
+                    if (newCastle == null)
+                    {
+                        Debug.LogError("移動先の城が見つかりません。");
+                        return;
+                    }
+                    castle.Members.Remove(chara);
+                    newCastle.Members.Add(chara);
+                    Save();
+                    LoadWorld();
+                    return;
+                }
+
+            }
+        }
+    }
+
+    private void DrawEditBuilding()
+    {
+        var hasCountry = targetCountry != null;
         if (hasCountry) countryIdForNewCastle = targetCountry.Id;
         if (targetTile.HasCastle) castleIdForNewCastle = targetTile.Castle.Id;
         if (targetTile.HasCastle) castleIdForNewTown = targetTile.Castle.Id;
@@ -186,12 +232,11 @@ public class TileInfoEditorWindow : EditorWindow
         {
             var castle = targetTile.Castle;
             // ヘッダー
-            EditorGUILayout.Space();
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label($"城情報 (ID: {castle.Id})", EditorStyles.boldLabel);
-            GUILayout.Label("X");
+            BoldLabel($"城情報 (ID: {castle.Id})");
+            Label("X");
             relocateX = EditorGUILayout.IntField(relocateX);
-            GUILayout.Label("Y");
+            Label("Y");
             relocateY = EditorGUILayout.IntField(relocateY);
             if (GUILayout.Button("再配置"))
             {
@@ -216,12 +261,12 @@ public class TileInfoEditorWindow : EditorWindow
 
             EditorGUILayout.BeginHorizontal();
             castle.Gold = EditorGUILayout.FloatField("Gold", castle.Gold);
-            GUILayout.Label($"{castle.GoldIncome} / {castle.GoldIncomeMax} ({castle.GoldBalance:+0;-#})");
+            Label($"{castle.GoldIncome} / {castle.GoldIncomeMax} ({castle.GoldBalance:+0;-#})");
             EditorGUILayout.EndHorizontal();
-                
+
             EditorGUILayout.BeginHorizontal();
             castle.Food = EditorGUILayout.FloatField("Food", castle.Food);
-            GUILayout.Label($"{castle.FoodIncome} / {castle.FoodIncomeMax}");
+            Label($"{castle.FoodIncome} / {castle.FoodIncomeMax}");
             EditorGUILayout.EndHorizontal();
 
             if (GUILayout.Button("城を削除"))
@@ -236,7 +281,7 @@ public class TileInfoEditorWindow : EditorWindow
         }
         else
         {
-            GUILayout.Label("城なし", EditorStyles.boldLabel);
+            BoldLabel("城なし");
             countryIdForNewCastle = EditorGUILayout.IntField("国ID", countryIdForNewCastle);
             castleIdForNewCastle = EditorGUILayout.IntField("新城ID", castleIdForNewCastle);
 
@@ -263,10 +308,9 @@ public class TileInfoEditorWindow : EditorWindow
         {
             var town = targetTile.Town;
             // ヘッダー
-            EditorGUILayout.Space();
-            GUILayout.Label($"町情報 (所属城ID: {town.Castle.Id})", EditorStyles.boldLabel);
+            BoldLabel($"町情報 (所属城ID: {town.Castle.Id})");
 
-            GUILayout.Label($"城主: {town.Castle.Boss?.Name ?? ""}");
+            Label($"城主: {town.Castle.Boss?.Name ?? ""}");
 
             EditorGUILayout.BeginHorizontal();
             town.GoldIncome = EditorGUILayout.FloatField("GoldIncome", town.GoldIncome);
@@ -289,7 +333,7 @@ public class TileInfoEditorWindow : EditorWindow
         }
         else
         {
-            GUILayout.Label("町なし", EditorStyles.boldLabel);
+            BoldLabel("町なし");
             castleIdForNewTown = EditorGUILayout.IntField("所属城ID", castleIdForNewTown);
             if (GUILayout.Button("町を作成"))
             {
@@ -308,18 +352,69 @@ public class TileInfoEditorWindow : EditorWindow
             }
         }
 
-
         ForceLayout();
+    }
+
+    /// <summary>
+    /// 施設の概要を描画します。
+    /// </summary>
+    private void DrawBuildingOverview()
+    {
+        if (targetCountry == null) return;
+
+        var ruler = targetCountry.Ruler;
+
+        GUILayout.BeginHorizontal();
+        using (VerticalLayout(GUILayout.Width(110)))
+        {
+            Label($"国: {targetCountry.Id} {ruler.Name}");
+            CharaImage(ruler);
+        }
+
+        if (targetTile.HasCastle)
+        {
+            var castle = targetTile.Castle;
+            using (VerticalLayout(GUILayout.Width(100)))
+            {
+                Label($"城: {castle.Id} {castle.Boss?.Name ?? ""}");
+                if (castle.Boss != null)
+                {
+                    CharaImage(castle.Boss);
+                }
+            }
+
+            using (VerticalLayout(GUILayout.Width(100)))
+            {
+                Label($"将数: {castle.Members.Count}");
+                using (HorizontalLayout())
+                {
+                    foreach (var chara in castle.Members.Where(m => m != castle.Boss))
+                    {
+                        SmallCharaImage(chara);
+                    }
+                }
+            }
+        }
+        GUILayout.EndHorizontal();
+        //var movings = targetTile.Castle?.Members.Where(m => m.IsMoving);
+        //if (movings != null)
+        //{
+        //    Label("出撃中: " + string.Join(", ", movings));
+        //}
+        //else
+        //{
+        //    GUILayout.Space(30);
+        //}
     }
 
     private int forceCreateCharacterId = -1;
     private MapPosition forceCreateDestination;
     private void ForceLayout()
     {
-        GUILayout.Label("軍勢追加", EditorStyles.boldLabel);
+        BoldLabel("軍勢追加");
         forceCreateCharacterId = EditorGUILayout.IntField("キャラID", forceCreateCharacterId);
         EditorGUILayout.BeginHorizontal();
-        GUILayout.Label("目標座標");
+        Label("目標座標");
         forceCreateDestination.x = EditorGUILayout.IntField("X", forceCreateDestination.x);
         forceCreateDestination.y = EditorGUILayout.IntField("Y", forceCreateDestination.y);
         EditorGUILayout.EndHorizontal();
@@ -347,11 +442,11 @@ public class TileInfoEditorWindow : EditorWindow
             return;
         }
 
-        GUILayout.Label("軍勢一覧", EditorStyles.boldLabel);
+        BoldLabel("軍勢一覧");
         foreach (var force in targetTile.Forces)
         {
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label($"{force.Character.Name} (To: {force.Destination}) move: {force.TileMoveRemainingDays}");
+            Label($"{force.Character.Name} (To: {force.Destination}) move: {force.TileMoveRemainingDays}", -1);
             if (GUILayout.Button("削除"))
             {
                 world.Forces.Unregister(force);
@@ -360,6 +455,35 @@ public class TileInfoEditorWindow : EditorWindow
             }
             EditorGUILayout.EndHorizontal();
         }
+    }
+
+    private static void SmallCharaImage(Character chara) => CharaImage(chara, 45);
+    private static void CharaImage(Character chara, int size = -1)
+    {
+        if (size == -1) size = 100;
+        var image = FaceImageManager.Instance.GetImage(chara);
+        GUILayout.Box(image, new GUIStyle()
+        {
+            fixedWidth = size,
+            fixedHeight = size,
+            margin = new(5, 5, 0, 0),
+        });
+    }
+
+    private static void BoldLabel(string label, int width = -1)
+    {
+        Label(label, width, EditorStyles.boldLabel);
+    }
+    private static void Label(string label, int width = -1, GUIStyle style = null)
+    {
+        style ??= EditorStyles.label;
+        if (width == -1)
+        {
+            GUILayout.Label(label, style);
+            return;
+        }
+
+        GUILayout.Label(label, style, GUILayout.Width(width));
     }
 
     private static void Delay(Action action, int delayMilliseconds = 300)
