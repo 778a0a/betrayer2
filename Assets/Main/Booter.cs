9e2beb0a13bad3dcd031f63e6e452c8f7dc87b0e
778a0a <778a0a@778a0a.com>
2025-10-30 23:01:39 +0900

    メッセージ大きさ調整

--- Assets/Main/Booter.cs
+++ Assets/Main/Booter.cs
@@ -96,7 +96,7 @@ public class Booter : MonoBehaviour
                 });
                 MessageWindow.Show(
                     "操作キャラを選択してください。\n\n" +
-                    "<color=#aaa>※マップ操作について<size=15>\n\n</size>" +
+                    "<color=#aaa>※マップ操作について<size=12>\n\n</size>" +
                     "・マウス中央ボタンドラッグでスクロールできます\n" +
                     "・マウスホイールでズームできます\n" +
                     "</color>");
